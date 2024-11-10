7728e9e09766be346b870d3fd2469af10a0de343
DimaYuzvak <demkaplay2004@gmail.com>
2024-11-10 16:11:32 +0200

    Catalog updated

--- LAB1/Controllers/CatalogController.cs
+++ LAB1/Controllers/CatalogController.cs
@@ -16,10 +16,53 @@ namespace LAB1.Controllers
             _context = context;
         }
 
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(int? authorId, int? illustratorId, string sortOrder, List<int> selectedTags, double? minRating)
         {
-            List<Manga> mangas = await _context.Manga.ToListAsync();
-            return View(mangas); // Передаємо список манг у представлення
+            var mangas = _context.Manga
+                .Include(m => m.Author)
+                .Include(m => m.Illustrator)
+                .Include(m => m.Tags)
+                .AsQueryable();
+
+            // Фільтрація за автором
+            if (authorId.HasValue)
+            {
+                mangas = mangas.Where(m => m.AuthorId == authorId.Value);
+            }
+
+            // Фільтрація за ілюстратором
+            if (illustratorId.HasValue)
+            {
+                mangas = mangas.Where(m => m.IllustratorId == illustratorId.Value);
+            }
+
+            // Фільтрація за тегами
+            if (selectedTags != null && selectedTags.Any())
+            {
+                mangas = mangas.Where(m => m.Tags.Any(t => selectedTags.Contains(t.Id)));
+            }
+
+            // Фільтрація за рейтингом
+            if (minRating.HasValue)
+            {
+                mangas = mangas.Where(m => m.AverageRating >= minRating.Value);
+            }
+
+            // Сортування
+            mangas = sortOrder switch
+            {
+                "title" => mangas.OrderBy(m => m.Title),
+                "year" => mangas.OrderByDescending(m => m.ReleaseYear),
+                "rating" => mangas.OrderByDescending(m => m.AverageRating),
+                "chapters" => mangas.OrderByDescending(m => m.Chapters),
+                _ => mangas.OrderBy(m => m.Title),
+            };
+
+            // Передаємо списки тегів, авторів та ілюстраторів у ViewBag
+            ViewBag.Tags = await _context.Tags.ToListAsync();
+            ViewBag.Authors = await _context.Authors.ToListAsync();
+
+            return View(await mangas.ToListAsync());
         }
     }
 }
